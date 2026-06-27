5e8b867b013b17075330db6b251fa6d2fdcf744c
Vihaan Aswal <vihaanaswal48@gmail.com>
2026-06-28 00:16:55 +0530

    @next/bundle-analyzer added and recharts downgraded to '2.15.0'

--- next.config.js
+++ next.config.js
@@ -109,4 +109,8 @@ const nextConfig = {
   productionBrowserSourceMaps: false,
 };
 
-module.exports = nextConfig;
+module.exports =
+  process.env.ANALYZE === "true"
+    ? // eslint-disable-next-line @typescript-eslint/no-require-imports
+      require("@next/bundle-analyzer")({ enabled: true })(nextConfig)
+    : nextConfig;
